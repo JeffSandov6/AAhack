24d44a88a8d0596d9a9559c9454a498da24f0eed
Muhammad Ashfaq <mohammad_great@hotmail.com>
2018-01-27 22:26:34 -0600

    Data Upload

--- tamuhackAA/app/src/main/java/com/example/mhashfaq/tamuhackaa/controllers/BaggageActivity.kt
+++ tamuhackAA/app/src/main/java/com/example/mhashfaq/tamuhackaa/controllers/BaggageActivity.kt
@@ -3,11 +3,45 @@ package com.example.mhashfaq.tamuhackaa.controllers
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mhashfaq.tamuhackaa.R
+import com.parse.ParseObject
+import java.util.*
+import kotlin.collections.ArrayList
 
 class BaggageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_baggage)
+
+        val list: List<String> = Arrays.asList("DFW", "HOU", "PHX", "DHM", "LAX", "DEN", "MIA", "MCO", "LAS", "JFK")
+        var depList : MutableList<String> = ArrayList()
+        var arrivalList : MutableList<String> = ArrayList()
+        var flightId : MutableList<String> = ArrayList()
+
+        for(item in list) {
+            var i = 0
+            while (i < 10) {
+                val randomizer = Random()
+                val random = list.get(randomizer.nextInt(list.size))
+                if (random == item) {
+                    continue
+                } else {
+                    depList.add(item)
+                    arrivalList.add(random)
+                    val flightIdString = item + "-" + random + "-12"
+                    if (!(flightIdString in flightId)) {
+                        flightId.add(flightIdString)
+                    }
+                    i++
+                }
+            }
+
+        }
+        for (i in depList.indices) {
+            var parseObject = ParseObject("bagData")
+            parseObject.put("departureAirport", depList[i])
+            parseObject.put("arrivalAirport", depList[i])
+            parseObject.saveInBackground()
+        }
     }
 }
--- tamuhackAA/app/src/main/java/com/example/mhashfaq/tamuhackaa/controllers/MainActivity.kt
+++ tamuhackAA/app/src/main/java/com/example/mhashfaq/tamuhackaa/controllers/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mhashfaq.tamuhackaa.controllers
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -26,7 +27,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             */
-
+            startActivity(Intent(baseContext, BaggageActivity::class.java))
         }
 
         flightAttendant.setOnClickListener {
